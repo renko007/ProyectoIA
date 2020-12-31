01adebf564ec837a827161a75a0e5c6764d57359
renko007 <gabriel.arayag@alumnos.usm.cl>
2020-12-31 17:14:17 -0300

    solucion inicial

--- proyecto.cpp
+++ proyecto.cpp
@@ -3,12 +3,17 @@
 #include<fstream>
 #include<string>
 #include<vector>
+#include <algorithm>
+#include <ctime>
 
 using namespace std;
+int T;
+int INTERVALO = 1;
+string instancia ="St.Andrews83.stu";//"EdHEC92.stu";
 
 void getSE(int *maxStu,int *maxE){
     std::ifstream archivo;
-    archivo.open("./instances/Carleton91.stu",ios::in);
+    archivo.open("./instances/"+instancia,ios::in);
     string a;
     int b,c; 
     *maxStu=0; 
@@ -30,7 +35,7 @@ void matrixSE(int **SE, int S, int E){
         }
     }
     std::ifstream archivo;
-    archivo.open("./instances/Carleton91.stu",ios::in);
+    archivo.open("./instances/"+instancia,ios::in);
     string estudiante;
     int examenID, estudianteID;
     while(archivo>>estudiante>>examenID)
@@ -71,11 +76,55 @@ void matrixC(int **matrizSE, int **C, int S,int E){
     }
 }
 
+bool checkeo(std::vector<int> x, int **C, int E){
+    int suma = 0;
+    for(int i=0; i <E-1;i++){
+        for (int j = i+1; j < E; j++){
+            if(x[i]!=0 && x[i]==x[j])
+                suma += C[i][j];
+        }
+    }
+    if(suma==0) return true;
+    else return false;
+}
+
+int numeroAleatorio (int max){
+    if (max <T) return (std::rand()%(max+INTERVALO)+1);
+    else return (std::rand()%T+1);
+}
+
+std::vector<int> solucionAleatoria(int E, int** C){
+    std::vector<int> v(E,0);
+    bool verificar;
+    int aux, max=5;
+    for(int i=0; i< v.size();i++){
+        do{
+            aux = numeroAleatorio(max);
+            v.at(i)= aux;
+            verificar = checkeo(v,C,E);
+        }while(!verificar);
+        if(max<aux) max = aux;
+    }
+    cout<<max<<endl;
+    return v;
+}
+
+void escribir(std::vector<int> v){
+    ofstream txtOut;
+    txtOut.open("soluciones.txt",ios::app);
+    txtOut<<"Nuevo:\n";
+    for (auto i : v) {
+        txtOut << i<<"\n";
+    }
+    txtOut.close();
+}
 
 int main(int argc, char const *argv[])
 {
+    std::srand(unsigned(std::time(nullptr)));
     int S,E;
     getSE(&S,&E);
+    T=E;
 
     int ** matrizSE = new int*[S];
     for (int i = 0; i<S;i++)
@@ -87,6 +136,14 @@ int main(int argc, char const *argv[])
         conflictMatrix[i] = new int[E];
     matrixC(matrizSE, conflictMatrix, S, E);
 
+    std::vector<int> x = solucionAleatoria(E, conflictMatrix);
+    escribir(x);
+
+
+    for (auto i : x) {
+        cout << i<<" ";
+    }
+    cout<<endl;
     // PRINT DE LA MATRIZ DE CONFLICTOS
     /*for(int i=0; i<E;i++){
         for(int j=0; j<E;j++){
